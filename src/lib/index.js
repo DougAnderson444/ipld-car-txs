13229a7fc21c04025875747493b835f81748548b
Doug A <douganderson444@gmail.com>
2022-08-24 12:56:42 -0300

    fix: default vs named import

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@
 export { default as CarTxs } from './CarTxs.svelte';
 export { default as Viz } from './Viz.svelte';
 export { default as Transaction } from './txs';
+export { default as default } from './txs';
