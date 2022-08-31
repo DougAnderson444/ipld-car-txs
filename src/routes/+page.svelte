2a5755d7038ffd41653ba165e708fc965af5ff05
Doug A <douganderson444@gmail.com>
2022-08-30 21:05:14 -0300

    add demo links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,18 @@
 	let commits: Uint8Array[] = [];
 
 	let data = { nodes: [], links: [] };
+
+	function createLink(e) {
+		console.log({ detail: e.detail });
+		const newLink = {
+			source: { id: e.detail.source.dataset.cid },
+			target: { id: e.detail.target.dataset.cid }
+		};
+		data.links = [...data.links, newLink];
+	}
+	$: data && console.log({ data });
 </script>
 
 <Canvas bind:data let:connectable>
-	<Board {commits} {connectable} />
+	<Board {commits} {connectable} on:createLink={createLink} />
 </Canvas>
